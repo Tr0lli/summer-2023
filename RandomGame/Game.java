3392ad9ea30dc072d24802b131609124e776509c
Francesco Trolli <franz.trolli@gmail.com>
2023-08-04 16:03:41 +0200

    Create new game

---
+++ RandomGame/Game.java
@@ -0,0 +1,8 @@
+package RandomGame;
+
+public class Game {
+    public static void main(String[] args) {
+        
+        new GameFrame();
+    }
+}
---
+++ RandomGame/GamePanel.java
@@ -0,0 +1,16 @@
+package RandomGame;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
+public class GamePanel extends JPanel implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
+    }
+    
+}
