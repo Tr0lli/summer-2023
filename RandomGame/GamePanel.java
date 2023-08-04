441a8c244d4413ff8c5de4b76c6fe815d6676c96
Francesco Trolli <franz.trolli@gmail.com>
2023-08-04 16:08:20 +0200

    Update GamePanel.java

--- RandomGame/GamePanel.java
+++ RandomGame/GamePanel.java
@@ -7,10 +7,36 @@ import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener{
 
+    static final int SCREEN_WIDTH = 1300;
+	static final int SCREEN_HEIGHT = 750;
+	static final int UNIT_SIZE = 50;
+	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/(UNIT_SIZE*UNIT_SIZE);
+	static final int DELAY = 125;
+    Random random;
+
+    GamePanel(){
+		random = new Random();
+		this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+		this.setBackground(Color.black);
+		this.setFocusable(true);
+		this.addKeyListener(new MyKeyAdapter());
+		startGame();
+	}
+
+    
+    private void startGame() {
+    }
+
+
+
     @Override
     public void actionPerformed(ActionEvent e) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
     }
     
+
+    public class MyKeyAdapter extends KeyAdapter{
+
+    }
 }
