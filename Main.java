df0d65d3b0dabc78d177c039a17b2327b398542d
Francesco Trolli <franz.trolli@gmail.com>
2023-08-11 11:31:17 +0200

    isPalindrome

--- Main.java
+++ Main.java
@@ -6,6 +6,27 @@ public class Main {
     public static void main(String[] args) {
         int a = 42;
         String a1 = "a";
-        System.out.println("Ho collegato GitHub" + a1.length());
+        System.out.println("Ho collegato GitHub " + isPalindrome(121));
+
+    }
+
+    public static boolean isPalindrome(int x) {
+        
+        String y = Integer.toString(x);
+        int n = y.length();
+        //System.out.println(n);
+
+        //int[] ax = new int[n];
+
+        boolean palindrome = true;
+       
+        for (int i=0; i<n/2; i++){
+            if (y.charAt(i) == y.charAt(n-1-i) && palindrome){
+                palindrome = true;
+            } else {
+                palindrome = false;
+            }
+        }
+        return palindrome;
     }
 }
