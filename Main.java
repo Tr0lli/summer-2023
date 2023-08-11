793930946d1009fe41b9e799c36bc98ada50dcec
Francesco Trolli <franz.trolli@gmail.com>
2023-08-11 10:25:15 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
+import java.util.List;
+
 public class Main {
 
      
     public static void main(String[] args) {
-        System.out.println("Ho collegato GitHub");
+        int a = 42;
+        String a1 = "a";
+        System.out.println("Ho collegato GitHub" + a1.length());
     }
 }
