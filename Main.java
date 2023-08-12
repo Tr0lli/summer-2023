f96cf6aa002cd67319f4dfa50a3e6f683efdaa8e
Francesco Trolli <franz.trolli@gmail.com>
2023-08-12 11:49:54 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,82 +6,86 @@ public class Main {
     public static void main(String[] args) {
         int a = 42;
         String a1 = "a";
-        System.out.println("Ho collegato GitHub " + isPalindrome(121));
+        System.out.println("Ho collegato GitHub " + isPalindrome(121) + " - "+ romanToInt("MCMXCIV"));
 
     }
 
     public static int romanToInt(String s) {
         int r = 0;
-        for (int i=0; i<s.length(); i++) {
-            if (s.charAt(i) == 'C' && s.charAt(i+1) == 'M'){
-                r = r - 100;
+        int i = 0;
+    
+        while (s.length() > i && s.charAt(i) == 'M'){
+            
+                r = r + 1000;
                 i++;
-            }
-            if (s.charAt(i) == 'M'){
-                while (s.charAt(i) == 'M'){
-                    r = r + 1000;
-                    i++;
-                }
-            }
-            if (s.charAt(i) == 'C' && s.charAt(i+1) == 'D'){
-                r = r - 100;
-                i++;
-            }
-            if (s.charAt(i) == 'D'){
-                while (s.charAt(i) == 'D'){
-                    r = r + 500;
-                    i++;
-                }
-            }
-            if (s.charAt(i) == 'X' && s.charAt(i+1) == 'C'){
-                r = r - 10;
-                i++;
-            }
-            if (s.charAt(i) == 'C'){
-                while (s.charAt(i) == 'C'){
-                    r = r + 100;
-                    i++;
-                }
-            }
-            if (s.charAt(i) == 'X' && s.charAt(i+1) == 'L'){
-                r = r - 10;
-                i++;
-            }
-            if (s.charAt(i) == 'L'){
-                while (s.charAt(i) == 'L'){
-                    r = r + 100;
-                    i++;
-                }
-            }
-            if (s.charAt(i) == 'I' && s.charAt(i+1) == 'X'){
-                r = r - 1;
-                i++;
-            }
-            if (s.charAt(i) == 'X'){
-                while (s.charAt(i) == 'X'){
-                    r = r + 10;
-                    i++;
-                }
-            }
-            if (s.charAt(i) == 'I' && s.charAt(i+1) == 'V'){
-                r = r - 1;
-                i++;
-            }
-            if (s.charAt(i) == 'X'){
-                while (s.charAt(i) == 'X'){
-                    r = r + 10;
-                    i++;
-                }
-            }
-            if (s.charAt(i) == 'I'){
-                int max = 0;
-                while (s.charAt(i) == 'I' && max<3){
-                    r = r + 1;
-                    i++;
-                    max++;
-                }
-            }
+            
         }
+        if (s.length() > i+1 && s.charAt(i) == 'C' && s.charAt(i+1) == 'M'){
+            r = r + 900;
+            i = i+2;
+        }
+        while (s.length() > i && s.charAt(i) == 'D'){
+            
+                r = r + 500;
+                i++;
+            
+        }
+        if (s.length() > i+1 && s.charAt(i) == 'C' && s.charAt(i+1) == 'D'){
+            r = r + 400;
+            i = i+2;
+        }
+        while (s.length() > i && s.charAt(i) == 'C'){
+            
+                r = r + 100;
+                i++;
+            
+        }
+        if (s.length() > i+1 && s.charAt(i) == 'X' && s.charAt(i+1) == 'C'){
+            r = r + 90;
+            i = i+2;
+        }
+        
+        while (s.length() > i && s.charAt(i) == 'L'){
+            
+                r = r + 50;
+                i++;
+            
+        }
+        if (s.length() > i+1 && s.charAt(i) == 'X' && s.charAt(i+1) == 'L'){
+            r = r + 40;
+            i = i+2;
+        }
+        
+        while (s.length() > i && s.charAt(i) == 'X'){
+            //while (s.charAt(i) == 'X'){
+                r = r + 10;
+                i++;
+            //}
+        }
+        if (s.length() > i+1 && s.charAt(i) == 'I' && s.charAt(i+1) == 'X'){
+            r = r + 9;
+            i = i+2;
+        }
+        
+        while (s.length() > i && s.charAt(i) == 'V'){
+            
+                r = r + 5;
+                i++;
+            
+        }
+        if (s.length() > i+1 && s.charAt(i) == 'I' && s.charAt(i+1) == 'V'){
+            r = r + 4;
+            i = i+2;
+        }
+        //if (s.length() > i && s.charAt(i) == 'I'){
+            int max = 0;
+            while (s.length() > i && s.charAt(i) == 'I' && max<3){
+                r = r + 1;
+                i++;
+                max++;
+            }
+        //}
+        
 
         return r;
         
